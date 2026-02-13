bd985200901cb52da44f900e37f9faf284af2786
henrynguy3n <bonnybon2006@gmail.com>
2026-02-14 10:14:15 +1100

    Update script.js

--- script.js
+++ script.js
@@ -56,14 +56,13 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "...",
+    "JUST SAY YES",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Okay fine.",
+    "no i see how it is"
 ];
 
 let messageIndex = 0;
